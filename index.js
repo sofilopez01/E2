fb03132a4995293e5e9653102adbba6f12146e48
sofia <sofiabelen648@gmail.com>
2023-12-01 21:39:37 -0300

    actualizacion

--- index.js
+++ index.js
@@ -43,7 +43,49 @@ const pizzas = [
   {
     id: 6,
     nombre: "pizza con Anana",
-    precio: 600,
+    precio: 60,
     ingredientes: ["Muzzarella", "Tomate", "Anana"],
   },
 ];
+
+//a)  Las pizzas que tengan un id impar.
+function IdImpar(pizzas) {
+  for (let i = 0; i < pizzas.length; i++) {
+    const pizza = pizzas[i];
+    if (pizza.id % 2 !== 0) {
+      console.log(`Pizza ID: ${pizzas[i].id}`);
+      console.log(`Nombre: ${pizzas[i].nombre}`);
+      console.log(`Precio: ${pizzas[i].precio}`);
+      console.log(`Ingredientes: ${pizzas[i].ingredientes.join(", ")}`);
+      //console.log("----------------------");
+    }
+  }
+}
+
+//IdImpar(pizzas);
+
+//b) Responder: ¿Hay alguna pizza que valga menos de $600?
+function pizzaMenor600(pizzas) {
+  for (let i = 0; i < pizzas.length; i++) {
+    const pizza = pizzas[i];
+    if (pizza.precio < 600) {
+      console.log(`Hay al menos una pizza que vale menos de $600`);
+      console.log(` La ${pizzas[i].nombre}, tiene un valor de $${pizzas[i].precio}`);
+      return;
+    }
+  }
+
+  console.log(`No hay ninguna pizza con precio menor a $600.`);
+}
+
+pizzaMenor600(pizzas);
+
+//c) El nombre de cada pizza con su respectivo precio.
+function NombreYPrecio(pizzas) {
+  for (let i = 0; i < pizzas.length; i++) {
+    const pizza = pizzas[i];
+    console.log(` ${pizza.nombre},  $${pizza.precio}`);
+  }
+}
+
+//NombreYPrecio(pizzas);
